22cb761bbe881175400ac446d0609ace6c5bd88f
Cire Jr <juniorcireba@gmail.com>
2025-02-14 14:26:31 +0000

    Update hero button links

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -16,16 +16,14 @@ export default function Hero() {
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
             className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/#solutions"
             rel="noopener noreferrer"
           >
             Explore Our Solutions
           </a>
           <a
             className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/contact"
             rel="noopener noreferrer"
           >
             Join the Innovation
