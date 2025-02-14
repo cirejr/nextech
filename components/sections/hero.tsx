02a79817ed69caa7eef98cb7e50af4e775f7722f
Cire Jr <juniorcireba@gmail.com>
2025-02-14 14:29:48 +0000

    Update hero.tsx

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -21,13 +21,12 @@ export default function Hero() {
           >
             Explore Our Solutions
           </a>
-          <a
-            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
+          <CustomLink
+            variant="secondary"
             href="/contact"
-            rel="noopener noreferrer"
           >
             Join the Innovation
-          </a>
+          </CustomLink>
         </div>
       </div>
     </PlusCornerSection>
